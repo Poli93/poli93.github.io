38dd0e59747d2c6ba4686802f43970e961c61234
Idan <idandayan@gmail.com>
2026-02-18 20:54:35 +0200

    deploy

---
+++ wotlk/bundle/index-c1affa49.entry.js
@@ -0,0 +1,2 @@
+import"./index-6d46f689.chunk.js";import{S as e,T as s}from"./detailed_results-85d44746.chunk.js";import{S as n,P as a}from"./preset_utils-e1524a50.chunk.js";import{H as t}from"./sim-a3578f70.chunk.js";const i=new n,o=new a(e.SpecHolyPaladin,i);o.enableHealing(),i.raid.setPlayer(s.nextEventID(),0,o),new t(document.body,o);
+//# sourceMappingURL=index-c1affa49.entry.js.map

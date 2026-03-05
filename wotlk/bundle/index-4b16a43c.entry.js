5a2cd43b90945199fbaf1b03355b251b4cf10120
Idan <idandayan@gmail.com>
2026-03-05 03:36:39 +0200

    deploy

---
+++ wotlk/bundle/index-4b16a43c.entry.js
@@ -0,0 +1,11 @@
+import "./index-6d46f689.chunk.js";
+import { S as Spec, T as TypedEvent } from "./detailed_results-4a530cbe.chunk.js";
+import { S as Sim, P as Player } from "./preset_utils-f9f053e5.chunk.js";
+import { T as TankDeathknightSimUI } from "./sim-3318e0c5.chunk.js";
+const index = "";
+const sim = new Sim();
+const player = new Player(Spec.SpecTankDeathknight, sim);
+player.enableHealing();
+sim.raid.setPlayer(TypedEvent.nextEventID(), 0, player);
+new TankDeathknightSimUI(document.body, player);
+//# sourceMappingURL=index-4b16a43c.entry.js.map

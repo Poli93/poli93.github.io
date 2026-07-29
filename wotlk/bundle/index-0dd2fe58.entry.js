a118462443efceb4ae8e0cf485909ce9c251ec7c
Poli93 <Poli93@users.noreply.github.com>
2026-07-29 11:20:46 +0000

    deploy: Poli93/wotlkpvpsim@37f45eb97b1f946a2a0318da82fd8e73a3acc95d

--- wotlk/bundle/index-0dd2fe58.entry.js
+++
@@ -1,2 +0,0 @@
-import"./index-6d46f689.chunk.js";import{S as e,T as s}from"./detailed_results-964b5b4f.chunk.js";import{S as t,P as a}from"./preset_utils-8a9ae3a4.chunk.js";import{F as n}from"./sim-6983afec.chunk.js";import"./suggest_gems_action-d486de26.chunk.js";import"./apl_utils-2ceb9fd2.chunk.js";const r=new t,i=new a(e.SpecFeralDruid,r);r.raid.setPlayer(s.nextEventID(),0,i),new n(document.body,i);
-//# sourceMappingURL=index-0dd2fe58.entry.js.map

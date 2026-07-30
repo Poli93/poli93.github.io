6f02cec5cc59a3c78ad1e2c080c97fcb9f4f55f5
Poli93 <Poli93@users.noreply.github.com>
2026-07-30 12:35:04 +0000

    deploy: Poli93/wotlkpvpsim@a3d0943386d19801fbafbc384ffbb47c24050b47

--- wotlk/bundle/index-04b22ac5.entry.js
+++
@@ -1,2 +0,0 @@
-import"./index-6d46f689.chunk.js";import{S as e,T as s}from"./detailed_results-964b5b4f.chunk.js";import{S as t,P as n}from"./preset_utils-0292ebe1.chunk.js";import{D as r}from"./sim-33a40cb7.chunk.js";const a=new t,i=new n(e.SpecDeathknight,a);a.raid.setPlayer(s.nextEventID(),0,i),new r(document.body,i);
-//# sourceMappingURL=index-04b22ac5.entry.js.map
---
+++ wotlk/bundle/index-922c471c.entry.js
@@ -0,0 +1,2 @@
+import"./index-6d46f689.chunk.js";import{S as e,T as s}from"./detailed_results-964b5b4f.chunk.js";import{S as t,P as n}from"./preset_utils-0292ebe1.chunk.js";import{D as r}from"./sim-62fbc1b9.chunk.js";const i=new t,o=new n(e.SpecDeathknight,i);i.raid.setPlayer(s.nextEventID(),0,o),new r(document.body,o);
+//# sourceMappingURL=index-922c471c.entry.js.map

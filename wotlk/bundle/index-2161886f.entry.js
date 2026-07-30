bde864f9ef2c8003b68672edc3a8fefd2e175dbc
Poli93 <Poli93@users.noreply.github.com>
2026-07-30 15:47:09 +0000

    deploy: Poli93/wotlkpvpsim@f9c7b1f890d48b3c3a8b12ab685e2d16b1dd9ef6

---
+++ wotlk/bundle/index-2161886f.entry.js
@@ -0,0 +1,2 @@
+import"./index-6d46f689.chunk.js";import{S as e,T as s}from"./detailed_results-964b5b4f.chunk.js";import{S as t,P as n}from"./preset_utils-0292ebe1.chunk.js";import{R as o}from"./sim-17cf50ba.chunk.js";const r=new t,a=new n(e.SpecRogue,r);r.raid.setPlayer(s.nextEventID(),0,a),new o(document.body,a);
+//# sourceMappingURL=index-2161886f.entry.js.map

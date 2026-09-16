5370d1f365d7377904ffe1ea761e170e72ec8096
Poli93 <Poli93@users.noreply.github.com>
2026-09-16 14:23:55 +0000

    deploy: Poli93/wotlkpvpsim@0faef9caccfbd46b73bf55429d58c4f5f636d8e2

---
+++ wotlk/bundle/index-189762a3.entry.js
@@ -0,0 +1,2 @@
+import"./index-6d46f689.chunk.js";import{S as e,T as s}from"./detailed_results-68178f52.chunk.js";import{S as t,P as n}from"./preset_utils-68f62323.chunk.js";import{W as r}from"./sim-058e6664.chunk.js";const o=new t,a=new n(e.SpecWarlock,o);o.raid.setPlayer(s.nextEventID(),0,a),new r(document.body,a);
+//# sourceMappingURL=index-189762a3.entry.js.map
